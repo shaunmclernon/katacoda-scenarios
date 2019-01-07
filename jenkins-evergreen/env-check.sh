b4d972e2d93943a961ed887abd3396b9ae38584c
McLernon, Shaun (ELS-CON) <s.mclernon@elsevier.com>
2019-01-07 17:45:12 +0000

    Updated

---
+++ jenkins-evergreen/env-check.sh
@@ -0,0 +1,3 @@
+clear
+while ((10)); do echo -e "\ec \u001b[32mPlease wait while we finish setting up your environment.\u001b[m"; sleep 1; done 2> /dev/null
+echo -e "\ec \u001b[32mYour Interactive Bash Terminal. A safe place to learn and execute commands.\u001b[m"
